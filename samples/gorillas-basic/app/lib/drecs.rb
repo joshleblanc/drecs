7b7d3de235b07f7582acfdfc07e85669987d2e56
Joshua.LeBlanc <jleblanc@omics.com>
2024-04-09 12:52:21 -0300

    helper methods & improve component performance

--- samples/gorillas-basic/app/lib/drecs.rb
+++ samples/gorillas-basic/app/lib/drecs.rb
@@ -15,58 +15,88 @@ module Drecs
 
   module ClassMethods
     def system(name, *filter, &blk)
-      SYSTEMS[name] = {
+      Drecs::SYSTEMS[name] = {
         components: filter,
         block: blk,
       }
     end
 
     def component(name, **defaults, &blk)
-      COMPONENTS[name] = {}
-      COMPONENTS[name].merge! defaults
+      Drecs::COMPONENTS[name] = {}
+      Drecs::COMPONENTS[name].merge! defaults
     end
 
     def entity(name, *components, **overrides)
-      ENTITIES[name] = {}
-      components.each { |c| ENTITIES[name][c] = {} }
-      overrides.each { |k, v| ENTITIES[name][k] = v }
+      Drecs::ENTITIES[name] = {}
+      components.each { |c| Drecs::ENTITIES[name][c] = {} }
+      overrides.each { |k, v| Drecs::ENTITIES[name][k] = v }
     end
   end
 
   def add_component(entity, component, **overrides)
-    c = entity[component] || COMPONENTS[component]
+    c = entity[component] || Drecs::COMPONENTS[component]
 
     entity[component] = c.merge(overrides)
+    entity.components << component unless entity.components.include? component
   end
 
   def remove_component(entity, component)
-    entity.as_hash.delete component
+    entity[component] = nil
+    entity.components.delete(component)
   end
 
   def has_components?(entity, *components)
-    keys = entity.as_hash.keys
     components.all? do |c|
-      keys.include? c
+      entity.components.include? c
     end
   end
 
-  def new_entity(name, **overrides)
-    $args.state.new_entity(name) do |e|
-      ENTITIES[name].each do |k, v|
+  def create_entity(name, **overrides)
+    unless Drecs::ENTITIES[name]
+      puts "No entity named #{name}"
+      return nil
+    end
+
+    $args.state.entities ||= []
+
+    state_alias = overrides.delete(:as)
+    entity = $args.state.new_entity(name) do |e|
+      e.alias = state_alias
+      e.components = []
+
+      Drecs::ENTITIES[name].each do |k, v|
         add_component(e, k, v.merge(overrides[k] || {}))
       end
     end
+    $args.state.entities << entity
+    $args.state[state_alias.to_sym] = entity if state_alias
+
+    entity
+  end
+
+  def delete_entity(entity)
+    $args.state.entities.delete(entity)
+    $args.state[entity.alias] = nil if entity.alias
+  end
+
+  def add_system(system)
+    $args.state.systems ||= []
+    $args.state.systems << system unless $args.state.systems.include?(system)
+  end
+
+  def remove_system(system)
+    $args.state.systems.delete(system)
   end
 
   def process_systems(args)
     args.state.systems.each do |system|
-      s = SYSTEMS[system]
-      s = SYSTEMS["#{system}_system".to_sym] unless s
+      s = Drecs::SYSTEMS[system]
+      s = Drecs::SYSTEMS["#{system}_system".to_sym] unless s
 
       next unless s
 
       system_entities = args.state.entities.select do |e|
-        next unless has_components?(e, s.components)
+        has_components?(e, *s.components)
       end
 
       instance_exec(system_entities, args, &s.block)
