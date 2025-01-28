54a7a0ff27fb9dc87b33778581fd3f1b33109594
Joshua.LeBlanc <jleblanc@omics.com>
2025-01-28 15:26:47 -0400

    get bind kind of working

--- pre.bat
+++ pre.bat
@@ -1,7 +1,7 @@
-set DRB_ROOT=..\
+set DRB_ROOT=..
 
 md native
 md native\windows-amd64
 git clone git@github.com:SanderMertens/flecs.git
-%DRB_ROOT%\dragonruby-bind.exe --compiler-flags="--sysroot=C:\mingw64" --output=native\ext-bind.c flecs\distr\flecs.c
-clang.exe -shared .\native\ext-bind.c --sysroot=C:\mingw64 --target=x86_64-w64-mingw32 -fuse-ld=lld -isystem %DRB_ROOT%\include -I. -o ../native\windows-amd64\ext.dll
+%DRB_ROOT%\dragonruby-bind.exe --compiler-flags="--sysroot=C:\mingw64 -IC:\LLVM\lib\clang\10.0.0\include -std=c99" --output=native\ext-bind.c flecs\distr\flecs.h
+clang.exe -shared .\native\ext-bind.c --sysroot=C:\mingw64 -IC:\LLVM\lib\clang\10.0.0\include -std=c99 --target=x86_64-w64-mingw32 -fuse-ld=lld -isystem %DRB_ROOT%\include -I. -o ../native\windows-amd64\ext.dll
