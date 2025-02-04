bcd6fe62735243bb064984e6fcef54dabce7e83a
Joshua <jleblanc@hey.com>
2025-02-03 20:28:45 -0400

    wip

--- app/ext.c
+++ app/ext.c
@@ -7,29 +7,26 @@
 
 static drb_api_t *drb_api;
 
-static void free_flecs_world(mrb_state *mrb, void *p)
-{
-  ecs_world_t *world = (ecs_world_t *)p;
-  free(world);
-}
-static const struct mrb_data_type flecs_world_data_type = { "ecs_world_t", free_flecs_world };
-static struct RClass *world_class;
-
-static void free_flecs_entity(mrb_state *mrb, void *p)
-{
-  ecs_entity_t *entity = (ecs_entity_t *)p;
-  free(entity);
-}
-static const struct mrb_data_type flecs_entity_data_type = { "ecs_entity_t", free_flecs_entity };
-static struct RClass *entity_class;
-
 
 static mrb_value flecs_ecs_init(mrb_state *mrb, mrb_value self) {
   ecs_world_t *p = ecs_init();
-  struct RData *d = drb_api->mrb_data_object_alloc(mrb, world_class, p, &flecs_world_data_type);
-  struct RBasic *world = (struct RBasic *)d;
-  return mrb_obj_value(world);
+
+  return drb_api->mrb_word_boxing_cptr_value(mrb, p);
 } 
+
+static mrb_value flecs_ecs_new(mrb_state *mrb, mrb_value self) {
+  mrb_value w = drb_api->mrb_get_arg1(mrb);
+  ecs_world_t *world = (ecs_world_t *)mrb_cptr(w);
+  ecs_entity_t *p = ecs_new(world);
+
+  return drb_api->mrb_word_boxing_cptr_value(mrb, p);
+} 
+
+static mrb_value flecs_ecs_get_scope(mrb_state *mrb, mrb_value self) {
+    mrb_value w = drb_api->mrb_get_arg1(mrb);
+    ecs_world_t *world = (ecs_world_t *)mrb_cptr(w);
+    return drb_api->mrb_word_boxing_cptr_value(mrb, ecs_get_scope(world));
+}
 
 DRB_FFI_EXPORT
 void drb_register_c_extensions_with_api(mrb_state *state, struct drb_api_t *api) {
@@ -38,11 +35,15 @@ void drb_register_c_extensions_with_api(mrb_state *state, struct drb_api_t *api)
   struct RClass *module = drb_api->mrb_define_module_under(state, FFI, "Flecs");
   struct RClass *base = state->object_class;
 
-  drb_api->mrb_define_module_function(state, module, "ecs_init", flecs_world_new, MRB_ARGS_REQ(0));
+  drb_api->mrb_define_module_function(state, module, "ecs_init", flecs_ecs_init, MRB_ARGS_REQ(0));
+  drb_api->mrb_define_module_function(state, module, "ecs_new", flecs_ecs_new, MRB_ARGS_REQ(1));
+  drb_api->mrb_define_module_function(state, module, "ecs_get_scope", flecs_ecs_get_scope, MRB_ARGS_REQ(1));
+  
+  // drb_api->mrb_define_module_function(state, module, "ecs_set_name", flecs_entity_new, MRB_ARGS_REQ(0));
 
-  world_class = drb_api->mrb_define_class_under(state, module, "World", base);
-  entity_class = drb_api->mrb_define_class_under(state, module, "Entity", base);
-  MRB_SET_INSTANCE_TT(world_class, MRB_TT_DATA);
-  drb_api->mrb_define_class_method(state, world_class, "new", flecs_world_new, MRB_ARGS_REQ(0));
-  drb_api->mrb_define_method(state, world_class, "entity", flecs_world_entity, MRB_ARGS_REQ(0));
+  // world_class = drb_api->mrb_define_class_under(state, module, "World", base);
+  // entity_class = drb_api->mrb_define_class_under(state, module, "Entity", base);
+  // MRB_SET_INSTANCE_TT(world_class, MRB_TT_DATA);
+  // drb_api->mrb_define_class_method(state, world_class, "new", flecs_world_new, MRB_ARGS_REQ(0));
+  // drb_api->mrb_define_method(state, world_class, "entity", flecs_world_entity, MRB_ARGS_REQ(0));
 }
--- samples/flecs/app/main.rb
+++ samples/flecs/app/main.rb
@@ -1,8 +1,21 @@
+class World 
+  def initialize
+    @flecs_world = FFI::Flecs.ecs_init
+  end
+
+  def entity 
+    FFI::Flecs.ecs_new(@flecs_world)
+  end
+
+  def scope 
+    FFI::Flecs.ecs_get_scope(@flecs_world)
+  end
+end
+
 def boot(args)
-  world = ecs_init
-  bob = ecs_set_name world, 0, "Bob"
-  
-  p world
+  world = World.new
+  p world.entity
+  p world.scope
 end
 
 def tick(args)
